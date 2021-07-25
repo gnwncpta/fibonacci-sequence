f7bdd0ee08f4fac7291b117722fd209f6dda1565
gnwncpta <gnwncpta@gmail.com>
2021-07-25 15:39:32 +0700

    fibonacci

--- fib.js
+++ fib.js
@@ -10,5 +10,10 @@ function Fibonacci(n){
     }
 }
 
-let res = [ Fibonacci(0), Fibonacci(1), Fibonacci(2), Fibonacci(3), Fibonacci(4), Fibonacci(5), Fibonacci(6), Fibonacci(7), Fibonacci(8) ];
+let res = [ Fibonacci(0), Fibonacci(1), Fibonacci(2), Fibonacci(3), Fibonacci(4), Fibonacci(5), Fibonacci(6), Fibonacci(7), Fibonacci(8), Fibonacci(9), Fibonacci(10) ];
 console.log(res);
+
+// Fibonacci(0) -> 0
+// Fibonacci(1) -> 1
+// Fiboancci(2) -> ( 2 - 1 ) + ( 2 - 2 ) = 1
+// Fibonacci(3) -> ( 3 - 1 ) + ( 3 - 2 ) = 3
